633e455d3bfeabe7d9e8f0b18e0bdc093946002e
MMashivskij <mmashivskij@gmail.com>
2019-10-09 23:26:53 +0300

    Solutions

--- src/connect.js
+++ src/connect.js
@@ -19,8 +19,18 @@
  * @param {Object} baseParams
  * @return {Function}
  */
-function connect(params) {
-  // write code here
+function connect(baseParams) {
+  const obj = { ...baseParams };
+  return (extraParams) => {
+    for (const key in extraParams) {
+      if (key in obj) {
+        obj[key] = extraParams[key];
+      }
+      obj[key] = extraParams[key];
+    }
+
+    return obj;
+  };
 }
 
 module.exports = connect;

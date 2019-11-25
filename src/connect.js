15ab5bceee7f744b99c76f5d725df15a6df87513
bandura <banduraweb@ukr.net>
2019-11-25 16:59:40 +0200

    Solution

--- src/connect.js
+++ src/connect.js
@@ -16,11 +16,15 @@
  * const result1 = connected1();
  * result1 is { x: 1, y: 2 }
  *
- * @param {Object} baseParams
+ * @param {Object} startsParams
  * @return {Function}
  */
-function connect(params) {
-  // write code here
-}
+
+const connect = startsParams => additionalParams => {
+  return {
+    ...startsParams,
+    ...additionalParams,
+  };
+};
 
 module.exports = connect;

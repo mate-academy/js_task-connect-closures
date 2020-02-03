748ec537f2f64aa616dc5b494e9454585d9c1ebe
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-02-03 17:42:02 +0200

    done

--- src/connect.js
+++ src/connect.js
@@ -20,7 +20,11 @@
  * @return {Function}
  */
 function connect(params) {
-  // write code here
+  return function(obj) {
+    return {
+      ...params, ...obj,
+    };
+  };
 }
 
 module.exports = connect;
